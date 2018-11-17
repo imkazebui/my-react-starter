6c916aa8a5cea7849aa5fa22be17ec90c9625892
I'mWind <buithanhphuong.it@gmail.com>
2018-11-18 02:53:09 +0700

    update image with ratio

--- src/modules/common/ImageInput/index.js
+++ src/modules/common/ImageInput/index.js
@@ -3,30 +3,30 @@ import { FormGroup, Label, Input, Row } from 'reactstrap';
 
 import styles from './styles.scss';
 
-const ImageInput = ({ image, label, onChange, isDisabled = false }) => {
+const ImageInput = ({ image, label, onChange, isDisabled = false, name = '', ratio = '100%' }) => {
 	if (!image) {
 		return (
 			<FormGroup id="choose-image">
 				<Label htmlFor={label}>
-					<p>Chọn hình ảnh để upload</p>
+					<p>Choose File to upload</p>
 				</Label>
-				<Input
-					required
-					type="file"
-					name={label}
-					id={label}
-					accept=".jpg, .jpeg, .png"
-					onChange={onChange}
-				/>
+				<Input required type="file" name={name} id={label} accept=".jpg, .jpeg, .png" onChange={onChange} />
 			</FormGroup>
 		);
 	}
 	return (
 		<FormGroup id="image-input">
-			<Label htmlFor={label}>
+			<Label htmlFor={label} style={{ position: 'relative', paddingTop: ratio }}>
 				<img src={image} alt="tb-image-input" />
 			</Label>
- 			<Input disabled={isDisabled} type="file" name={label} id={label} accept=".jpg, .jpeg, .png" onChange={onChange} />
+			<Input
+				disabled={isDisabled}
+				type="file"
+				name={name}
+				id={label}
+				accept=".jpg, .jpeg, .png"
+				onChange={onChange}
+			/>
 		</FormGroup>
 	);
 };
